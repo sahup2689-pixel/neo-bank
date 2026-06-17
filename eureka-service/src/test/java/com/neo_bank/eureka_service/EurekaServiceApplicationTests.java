9295fed5f3f51ed8905ee6746137b1a2456f881d
ptannu22 <ptarannum4435@gmail.com>
2026-06-17 16:39:21 +0530

    Add eureka server

---
+++ eureka-service/src/test/java/com/neo_bank/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo_bank.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
