6e4f7ee03e210a039b699f28e5c072728e238156
Pratyush Sahu <sahup2689@gmail.com>
2026-06-17 19:28:57 +0530

    Added Config Server

---
+++ config-server/src/test/java/Com/neo_bank/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.neo_bank.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
