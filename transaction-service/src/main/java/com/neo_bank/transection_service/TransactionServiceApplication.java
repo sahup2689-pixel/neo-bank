7e058e9c98fd9de6768ae91fb121ee9a14189257
Pratyush Sahu <sahup2689@gmail.com>
2026-07-11 12:48:06 +0530

    Created transaction service

---
+++ transaction-service/src/main/java/com/neo_bank/transection_service/TransactionServiceApplication.java
@@ -0,0 +1,14 @@
+package com.neo_bank.transection_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class TransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+}
---
+++ transaction-service/src/test/java/com/neo_bank/transection_service/TransectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo_bank.transection_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
