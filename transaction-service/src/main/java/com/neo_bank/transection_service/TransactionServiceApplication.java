3159803c1005eac985dbe65677392b5fa7ffdeb5
Pratyush Sahu <sahup2689@gmail.com>
2026-07-13 12:13:39 +0530

    Updated Transection Service

--- transaction-service/src/main/java/com/neo_bank/transection_service/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/neo_bank/transection_service/TransactionServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TransactionServiceApplication.class, args);
 	}
 }
